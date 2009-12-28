756846c7e433892d6ead544d17b45042a1ba546f
Robert Carr <racarr@gnome.org>
2009-12-28 01:37:05 -0500

    Add a red rectangle

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <clutter/clutter.h>
 #include <stdlib.h>
 
+void
+add_rectangle_one (ClutterActor *stage)
+{
+  ClutterColor red = {0xff, 0x00, 0x00, 0xff};
+  //Don't need to use typesafe cast, since we know it will be a rectangle
+  ClutterActor *rect = clutter_rectangle_new ();
+  
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage), rect);
+  
+  clutter_actor_set_size (rect, 100, 100);
+  clutter_actor_set_position (rect, 0, 0);
+  
+  clutter_rectangle_set_color (CLUTTER_RECTANGLE (rect), &red);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -8,6 +23,9 @@ main (int argc, char **argv)
   clutter_init (&argc, &argv);
   
   stage = clutter_stage_new ();
+
+  add_rectangle_one (stage);
+
   clutter_actor_show (stage);
   
   clutter_main ();
