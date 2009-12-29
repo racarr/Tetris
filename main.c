ac85615c956368123d1a20542f9cb217e7330696
Robert Carr <racarr@gnome.org>
2009-12-29 02:07:05 -0500

    Add make button skeleton

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <clutter/clutter.h>
 #include <stdlib.h>
 
+#define STAGE_WIDTH 500
+#define STAGE_HEIGHT 500
+
+#define BUTTON_SIZE 50
+
+void
+setup_stage (ClutterActor *stage)
+{
+  ClutterColor black = {0x00, 0x00, 0x00, 0xff};
+
+  clutter_actor_set_size (stage, STAGE_WIDTH, STAGE_HEIGHT);
+  clutter_stage_set_color (CLUTTER_STAGE (stage), &black);
+}
+
+ClutterActor *
+make_button (ClutterColor *color, ClutterColor *border_color)
+{
+  ClutterActor *but;
+  
+  
+  return but;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -8,6 +31,7 @@ main (int argc, char **argv)
   clutter_init (&argc, &argv);
   
   stage = clutter_stage_new ();
+  setup_stage (stage);
 
   clutter_actor_show (stage);
   
