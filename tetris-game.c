1d5ce21e6da89fc4c2378d66fff03ddf3599feb0
Robert Carr <racarr@gnome.org>
2010-01-05 01:10:20 -0500

    More implementation comments

--- tetris-game.c
+++ tetris-game.c
@@ -21,22 +21,50 @@
 TetrisGame *
 tetris_game_new (void)
 {
+  TetrisGame *ret;
   
+  /* Allocate new game */
+  /* Zero board array */
+  
+  return ret;
 }
 
 void
 tetris_game_drop_all_above (TetrisGame *game, guint row)
 {
+  /* Is game non null */
+  /* Zero everything at Y=row + free blocks */
+  /* Go through all Y's above row:
+        if board member is non null (there is a block there):
+    	   move down + update y coord in block
+  */
 }
 
 gboolean
 tetris_game_move_is_acceptable (TetrisGame *game, TetrisBlock *block, TetrisBlock *new_block)
 {
+  /* Is game non null */
+  /* For every block in new block connections:
+         is y < 0 or x < 0 or x >= TILE_WIDTH? return false
+         is the block at x/y != NULL and not connected to our block?
+         return false */
+  /* Return true */
 }
 
 gboolean
 tetris_game_move_block (TetrisGame *game, TetrisBlock *block, TetrisDirection direction)
 {
+  /* Is block non null */
+  gint dx, dy = 0;
+  TetrisBlock *test_block;
+  
+  
+  /* Compute dx and dy based on direction */
+  /* Copy block to test block */
+  /* Translate all blocks connected to test block by dx/dy */
+  /* Is test_block an acceptable move? if not return false, otherwise
+     perform translation on actual block + update game board */
+  return TRUE;
 }
 
 gboolean 
