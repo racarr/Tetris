d1760a3b6186f78051585351d645391f4a63da7f
Robert Carr <racarr@gnome.org>
2010-01-08 23:45:23 -0500

    Implement tetris_create_I

---
+++ tetris-util.c
@@ -0,0 +1,82 @@
+/*
+ * This file is part of a Clutter based Tetris implementation.
+ *
+ * tetris-util.c is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * tetris-util.c is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with tetris-util.c.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) Robert Carr 2009 <racarr@gnome.org>
+ * Copyright (C) Amy Wielizcka 2009 <amy.wielizcka@gmail.com>
+ */
+
+#include "tetris.h"
+
+/*
+ * tetris_interconnect_blocks:
+ * @blocks: An array of blocks to interconnect.
+ * @num_blocks: The number of blocks in the @blocks array.
+ *
+ * Takes an array of blocks (@blocks) and puts each #TetrisBlock in
+ * each other blocks list of connections, including itself.
+ */
+void
+tetris_interconnect_blocks (TetrisBlock **blocks, guint num_blocks)
+{
+}
+
+TetrisBlock *
+tetris_create_I (guint x, guint y)
+{
+  TetrisBlock *blocks[] = 
+    {
+      tetris_block_new (x, y, TETRIS_BLOCK_I, FALSE),
+      tetris_block_new (x+1, y, TETRIS_BLOCK_I, TRUE),
+      tetris_block_new (x+2, y, TETRIS_BLOCK_I, FALSE),
+      tetris_block_new (x+3, y, TETRIS_BLOCK_I, FALSE)
+    };
+  tetris_interconnect_blocks (blocks, 4);
+  
+  return blocks[0];
+}
+
+TetrisBlock *
+tetris_create_J (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_L (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_O (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_S (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_T (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_Z (guint x, guint y)
+{
+}
+
+TetrisBlock *
+tetris_create_random (guint x, guint y)
+{
+}
